ce0c15e492939c256308e6d630e0c53dd85a1c9b
hnsheth07 <hnsheth@andrew.cmu.edu>
2019-10-28 13:51:24 -0400

    Added code for lunar surface with robot model and sensors

---
+++ elevationmap.m
@@ -0,0 +1,23 @@
+clear all
+close all
+% load('heightmap.mat')
+% side = size(heightmap,2);
+% length = round(sqrt(side));
+% heightmap = reshape(heightmap, [length, length]);
+
+
+% heightmap = csvread('sldem_snippet_with_rocks.csv');
+% I = mat2gray(heightmap);
+% figure
+% imshow(I)
+% [r, c] = size(heightmap);
+% 
+% flatten_heightmap = reshape(heightmap, [1, r*c]);
+% csvwrite('heightmap.csv',flatten_heightmap);
+
+t = Tiff('worlds/NAC_DTM_TSILKVSKIY2_CLRSHADE_20170911_164042.tif', 'r');
+imageData = read(t);
+[r, c] = size(imageData);
+flatten_heightmap = reshape(imageData, [1, r*c]);
+csvwrite('heightmap.csv',flatten_heightmap);
+
