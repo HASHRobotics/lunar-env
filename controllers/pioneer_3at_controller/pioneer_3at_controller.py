0aff2bb285e175567a4e64aade3d92fbcc2d1a0b
hnsheth07 <hnsheth@andrew.cmu.edu>
2019-10-31 21:12:24 -0400

    Added teleop robot

--- controllers/pioneer_3at_controller/pioneer_3at_controller.py
+++ controllers/pioneer_3at_controller/pioneer_3at_controller.py
@@ -24,6 +24,9 @@ from std_msgs.msg import Float64
 from controller import Robot, Camera, Keyboard
 import os
 
+SPEED_UNIT = 0.0628
+INCR = 10
+
 class CustomRobotClass:
 
     def __init__(self):
@@ -39,7 +42,7 @@ class CustomRobotClass:
         # rangeFinder = robot.getRangeFinder("MultiSense S21 meta range finder")
         self.leftpositionsensor = self.robot.getPositionSensor('back left wheel sensor')
         self.rightpositionsensor = self.robot.getPositionSensor('back right wheel sensor')
-
+        self.keyboard = Keyboard()
 
         # Enable sensors
         # metacamera.enable(timeStep)
@@ -48,15 +51,20 @@ class CustomRobotClass:
         # rangeFinder.enable(timeStep)
         self.leftpositionsensor.enable(self.time_step)
         self.rightpositionsensor.enable(self.time_step)
+        self.keyboard.enable(self.time_step)
 
         # Initialize wheels
         backrightwheel = self.robot.getMotor('back right wheel')
         backleftwheel = self.robot.getMotor('back left wheel')
-        frontrigthwheel = self.robot.getMotor('front right wheel')
+        frontrightwheel = self.robot.getMotor('front right wheel')
         frontleftwheel = self.robot.getMotor('front left wheel')
-        self.wheels = [backrightwheel, backleftwheel, frontrigthwheel, frontleftwheel]
+        self.wheels = [backrightwheel, backleftwheel, frontrightwheel, frontleftwheel]
 
         self.set_velocity_control()
+        self.prevlspeed = 0
+        self.prevrspeed = 0
+        self.curlspeed = 0
+        self.currspeed = 0
 
     def set_velocity_control(self):
         for wheel in self.wheels:
@@ -65,6 +73,24 @@ class CustomRobotClass:
 
     def step(self):
         self.robot.step(self.time_step)
+
+    def set_speed(self, l, r):
+        if self.prevlspeed !=l or self.prevrspeed != r:
+            self.wheels[0].setVelocity(SPEED_UNIT*r)
+            self.wheels[1].setVelocity(SPEED_UNIT*l)
+            self.wheels[2].setVelocity(SPEED_UNIT*r)
+            self.wheels[3].setVelocity(SPEED_UNIT*l)
+            self.prevlspeed = self.curlspeed
+            self.prevrspeed = self.currspeed
+            self.curlspeed = l
+            self.currspeed = r
+            print("Speed",l,r)
+
+# class Odometry:
+
+#     def __init__(self)
+
+
 
 class RosNode:
     def __init__(self):
@@ -75,10 +101,26 @@ class RosNode:
 
 def run_robot(ros_node, robot):
     while custom_robot.step() != -1 and not rospy.is_shutdown():
+        l = robot.curlspeed
+        r = robot.currspeed
+        key = robot.keyboard.getKey()
+        if key == Keyboard.UP:
+            l += INCR
+            r += INCR
+        elif key == Keyboard.DOWN:
+            l -= INCR
+            r -= INCR
+        elif key == Keyboard.LEFT:
+            r += INCR
+            l -= INCR
+        elif key == Keyboard.RIGHT:
+            r -= INCR
+            l += INCR
+        robot.set_speed(l,r)
         ros_node.right_encoder_publisher.publish(robot.rightpositionsensor.getValue())
         ros_node.left_encoder_publisher.publish(robot.leftpositionsensor.getValue())
-        for wheel in robot.wheels:
-            wheel.setVelocity(5)
+        # for wheel in robot.wheels:
+            # wheel.setVelocity(-5)
 
 
 # def callback(data):
