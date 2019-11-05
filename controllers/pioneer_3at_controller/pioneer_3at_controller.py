148111c1262acf7dc50b67cb97308ff0345e1e7f
hnsheth07 <hnsheth@andrew.cmu.edu>
2019-11-05 10:43:44 -0500

    Corrected odometry error, published odometry transform, added code for consuming cmd_vel

--- controllers/pioneer_3at_controller/pioneer_3at_controller.py
+++ controllers/pioneer_3at_controller/pioneer_3at_controller.py
@@ -31,6 +31,7 @@ from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 SPEED_UNIT = 0.0628
 INCR = 10
 ENCODER_UNIT = 159.23
+WHEEL_BASE = 0.25
 
 #TODO: Change variables for pioneer 3AT
 axis_wheel_ratio = 1
@@ -97,7 +98,7 @@ class CustomRobotClass:
             self.prevrspeed = self.currspeed
             self.curlspeed = l
             self.currspeed = r
-            print("Speed",l,r)
+            # print("Speed",l,r)
 
 class OdometryClass:
 
@@ -116,72 +117,118 @@ class OdometryClass:
         self.y = 0
         self.theta = 0
 
-class OdometryGotoClass:
-    def __init__(self):
-        self.speed_min = 1
-        self.goal_x = 0
-        self.goal_y = 0
-        self.goal_theta = 0
-        self.speed_left = 0
-        self.speed_right = 0
-        self.atgoal = 1
+    def odometry_track_step_pose(self, pos_left, pos_right):
+        delta_pos_left = pos_left - self.pos_left_prev;
+        delta_pos_right = pos_right - self.pos_right_prev;
 
-    def set_goal(self, x, y, theta):
-        self.goal_x = x
-        self.goal_y = y
-        self.goal_theta = theta
-        self.atgoal = 0
+        delta_left = delta_pos_left * self.wheel_conversion_left
+        delta_right = delta_pos_right * self.wheel_conversion_right
+        delta_theta = (delta_right - delta_left) / self.wheel_distance
+        theta2 = self.theta + delta_theta * 0.5;
+        delta_x = (delta_left + delta_right) * 0.5 * math.cos(theta2)
+        delta_y = (delta_left + delta_right) * 0.5 * math.sin(theta2)
 
-    def step(self, odometry):
-        cur_position = [odometry.x, odometry.y, odometry.theta]
-        goal_position = [self.goal_x, self.goal_y, self.goal_theta]
+        self.x += delta_x
+        self.y += delta_y
+        self.theta += delta_theta
+        if self.theta > math.pi:
+            self.theta -= 2*math.pi
+        if self.theta < -math.pi:
+            self.theta += 2*math.pi
 
-        dx = goal_position[0] - cur_position[0]
-        dy = goal_position[1] - cur_position[1]
+        self.pos_left_prev = pos_left;
+        self.pos_right_prev = pos_right;
 
-        # Gain parameters
-        k_rho = 1.2
-        k_alpha = 1.5
-        k_beta = -0.35
+    def odometry_start_pos(self, pos_left, pos_right):
+        self.x = 0
+        self.y = 0
+        self.theta = 0
 
-        v_adapt = 1000/0.13
-        w_adapt = 2000/ (270 * math.pi/180)
+        self.pos_left_prev = pos_left
+        self.pos_right_prev = pos_right
 
-        rho_c = math.sqrt(dx*dx + dy*dy)
-        alpha_c = math.atan2(dy, dx) - cur_position[2]
-        while alpha_c > math.pi:
-            alpha_c = alpha_c - 2 * math.pi;
-        while alpha_c < -math.pi:
-            alpha_c = alpha_c + 2 * math.pi;
+        self.wheel_distance = axis_wheel_ratio*scaling_factor*(wheel_diameter_left + wheel_diameter_right)/2
+        self.wheel_conversion_left = wheel_diameter_left * scaling_factor * math.pi / increments_per_tour
+        self.wheel_conversion_right = wheel_diameter_right * scaling_factor * math.pi / increments_per_tour
 
-        beta_c = -cur_position[2] - alpha_c
-        while alpha_c > math.pi:
-            beta_c = beta_c - 2 * math.pi;
-        while alpha_c < -math.pi:
-            beta_c = beta_c + 2 * math.pi;
+# class OdometryGotoClass:
 
-        # Control Law
-        v_c = k_rho * rho_c
-        w_c = k_alpha * alpha_c + k_beta * beta_c
+#     def __init__(self):
+#         self.speed_min = 1
+#         self.goal_x = 0
+#         self.goal_y = 0
+#         self.goal_theta = 0
+#         self.speed_left = 0
+#         self.speed_right = 0
+#         self.atgoal = 1
 
-        # Robot units
-        v_e = v_c * v_adapt
-        w_e = w_c * w_adapt
+#     def set_goal(self, x, y, theta):
+#         self.goal_x = x
+#         self.goal_y = y
+#         self.goal_theta = theta
+#         self.atgoal = 0
+
+#     def step(self, odometry):
+#         cur_position = [odometry.x, odometry.y, odometry.theta]
+#         goal_position = [self.goal_x, self.goal_y, self.goal_theta]
+
+#         dx = goal_position[0] - cur_position[0]
+#         dy = goal_position[1] - cur_position[1]
+
+#         # Gain parameters
+#         k_rho = 1.2
+#         k_alpha = 1.5
+#         k_beta = -0.35
+
+#         v_adapt = 1000/0.13
+#         w_adapt = 2000/ (270 * math.pi/180)
+
+#         rho_c = math.sqrt(dx*dx + dy*dy)
+#         alpha_c = math.atan2(dy, dx) - cur_position[2]
+#         while alpha_c > math.pi:
+#             alpha_c = alpha_c - 2 * math.pi;
+#         while alpha_c < -math.pi:
+#             alpha_c = alpha_c + 2 * math.pi;
+
+#         beta_c = -cur_position[2] - alpha_c
+#         while alpha_c > math.pi:
+#             beta_c = beta_c - 2 * math.pi;
+#         while alpha_c < -math.pi:
+#             beta_c = beta_c + 2 * math.pi;
+
+#         # Control Law
+#         v_c = k_rho * rho_c
+#         w_c = k_alpha * alpha_c + k_beta * beta_c
+
+#         # Robot units
+#         v_e = v_c * v_adapt
+#         w_e = w_c * w_adapt
 
 
-        speed_left = (v_e - w_e/2)
-        speed_right = (v_e + w_e/2)
+#         speed_left = (v_e - w_e/2)
+#         speed_right = (v_e + w_e/2)
 
-        if (speed_right == 0 and speed_left == 0) or rho_c < 0.002:
-            self.atgoal = 1
+#         if (speed_right == 0 and speed_left == 0) or rho_c < 0.002:
+#             self.atgoal = 1
 
-        return [speed_left, speed_right]
+#         return [speed_left, speed_right]
+
+def command_velocity_callback(data):
+    global left_velocity
+    global right_velocity
+    robot_linear_velocity = data.linear.x
+    robot_angular_velocity = data.angular.z
+
+    left_velocity = robot_linear_velocity - 0.5*robot_angular_velocity*WHEEL_BASE
+    right_velocity = robot_linear_velocity + 0.5*robot_angular_velocity*WHEEL_BASE
+
 
 class RosNode:
     def __init__(self):
         self.right_encoder_publisher = rospy.Publisher('rwheel', Float64, queue_size=10)
         self.left_encoder_publisher = rospy.Publisher('lwheel', Float64, queue_size=10)
         self.odometry_publisher = rospy.Publisher('odometry', Odometry, queue_size=100)
+        rospy.Subscriber('cmd_vel', Twist, command_velocity_callback)
 
     def publish_odometry(self, odometry):
         odom_quat = tf.transformations.quaternion_from_euler(0, 0, odometry.theta)
@@ -210,89 +257,53 @@ class RosNode:
         )
 
 
-def odometry_start_pos(odometry, pos_left, pos_right):
-    odometry.x = 0
-    odometry.y = 0
-    odometry.theta = 0
-
-    odometry.pos_left_prev = pos_left
-    odometry.pos_right_prev = pos_right
-
-    odometry.wheel_distance = axis_wheel_ratio*scaling_factor*(wheel_diameter_left + wheel_diameter_right)/2
-    odometry.wheel_conversion_left = wheel_diameter_left * scaling_factor * math.pi / increments_per_tour
-    odometry.wheel_conversion_right = wheel_diameter_right * scaling_factor * math.pi / increments_per_tour
-    return odometry
-
-def odometry_track_step_pose(odometry, pos_left, pos_right):
-    delta_pos_left = pos_left - odometry.pos_left_prev;
-    delta_pos_right = pos_right - odometry.pos_right_prev;
-
-    delta_left = delta_pos_left * odometry.wheel_conversion_left
-    delta_right = delta_pos_right * odometry.wheel_conversion_right
-    delta_theta = (delta_right - delta_left) / odometry.wheel_distance
-    theta2 = odometry.theta + delta_theta * 0.5;
-    delta_x = (delta_left + delta_right) * 0.5 * math.cos(theta2)
-    delta_y = (delta_left + delta_right) * 0.5 * math.sin(theta2)
-
-    odometry.x += delta_x
-    odometry.y += delta_y
-    odometry.theta += delta_theta
-    if odometry.theta > math.pi:
-        odometry.theta -= 2*math.pi
-    if odometry.theta < -math.pi:
-        odometry.theta += 2*math.pi
-
-    odometry.pos_left_prev = pos_left;
-    odometry.pos_right_prev = pos_right;
-
-    return odometry
-
-def goto_position(x,y,theta, odometry, robot):
-    odometrygoto = OdometryGotoClass()
-    odometrygoto.set_goal(x, y, theta)
-    while odometrygoto.atgoal == 0:
-        pos_left = ENCODER_UNIT*robot.leftpositionsensor.getValue()
-        pos_right = ENCODER_UNIT*robot.rightpositionsensor.getValue()
-        odometry = odometry_track_step_pose(odometry, pos_left, pos_right)
-        [speed_left, speed_right] = odometrygoto.step(odometry)
-        robot.set_speed(speed_left, speed_right)
+# def goto_position(x,y,theta, odometry, robot):
+#     odometrygoto = OdometryGotoClass()
+#     odometrygoto.set_goal(x, y, theta)
+#     while odometrygoto.atgoal == 0:
+#         pos_left = ENCODER_UNIT*robot.leftpositionsensor.getValue()
+#         pos_right = ENCODER_UNIT*robot.rightpositionsensor.getValue()
+#         odometry.odometry_track_step_pose(pos_left, pos_right)
+#         [speed_left, speed_right] = odometrygoto.step(odometry)
+#         robot.set_speed(speed_left, speed_right)
 
 
 def run_robot(ros_node, robot):
+    global left_velocity
+    global right_velocity
     odometry = OdometryClass()
-
     pos_left = ENCODER_UNIT*robot.leftpositionsensor.getValue()
     pos_right = ENCODER_UNIT*robot.rightpositionsensor.getValue()
-    odometry = odometry_start_pos(odometry, pos_left, pos_right)
+    print(robot.leftpositionsensor.getValue(), robot.rightpositionsensor.getValue())
+    odometry.odometry_start_pos(pos_left, pos_right)
     while custom_robot.step() != -1 and not rospy.is_shutdown():
-        l = robot.curlspeed
-        r = robot.currspeed
+        print("I am here")
+        left_velocity = robot.curlspeed
+        right_velocity = robot.currspeed
         key = robot.keyboard.getKey()
         if key == Keyboard.UP:
-            l += INCR
-            r += INCR
+            left_velocity += INCR
+            right_velocity += INCR
         elif key == Keyboard.DOWN:
-            l -= INCR
-            r -= INCR
+            left_velocity -= INCR
+            right_velocity -= INCR
         elif key == Keyboard.LEFT:
-            r += INCR
-            l -= INCR
+            right_velocity += INCR
+            left_velocity -= INCR
         elif key == Keyboard.RIGHT:
-            r -= INCR
-            l += INCR
-        robot.set_speed(l,r)
+            right_velocity -= INCR
+            left_velocity += INCR
+        robot.set_speed(left_velocity,right_velocity)
         ros_node.right_encoder_publisher.publish(robot.rightpositionsensor.getValue())
         ros_node.left_encoder_publisher.publish(robot.leftpositionsensor.getValue())
-        # for wheel in robot.wheels:
-            # wheel.setVelocity(-5)
 
         pos_left = ENCODER_UNIT*robot.leftpositionsensor.getValue()
         pos_right = ENCODER_UNIT*robot.rightpositionsensor.getValue()
-        odometry = odometry_track_step_pose(odometry, pos_left, pos_right)
+        odometry.odometry_track_step_pose(pos_left, pos_right)
+
         ros_node.publish_odometry(odometry)
         # Go to Position
-        goto_position(0, 0.2, math.pi, odometry, robot);
-
+        # goto_position(0, 0.2, math.pi, odometry, robot);
 
 # def callback(data):
 #     global velocity
